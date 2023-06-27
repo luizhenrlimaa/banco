43ea96d9bb03d8720dc2a67af4cf281489049a47
Luiz Henrique Lima <luizhenrlimaa@gmail.com>
2023-06-27 13:52:25 -0300

    Fazendo uso de struct

--- src/main.go
+++ src/main.go
@@ -2,18 +2,13 @@ package main
 
 import "fmt"
 
+type ContaCorrente struct {
+	titular    string
+	numAgencia int
+	numConta   int
+	saldo      float64
+}
+
 func main() {
-	titular := "Luiz"
-	numAgencia := 589
-	numConta := 123456
-	var saldo float64 = 125.50
-
-	fmt.Println(titular, numAgencia, numConta, saldo)
-
-	titular2 := "Luciene"
-	numAgencia2 := 555
-	numConta2 := 1113333
-	var saldo2 float64 = 200
-
-	fmt.Println(titular2, numAgencia2, numConta2, saldo2)
+	fmt.Println(ContaCorrente{})
 }
