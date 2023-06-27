70a4e92893f9a03a9f5b989db99d8f703371949c
Luiz Henrique Lima <luizhenrlimaa@gmail.com>
2023-06-27 13:38:05 -0300

    Criacao do projeto

---
+++ src/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	titular := "Luiz"
+	numAgencia := 589
+	numConta := 123456
+	var saldo float64 = 125.50
+
+	fmt.Println(titular, numAgencia, numConta, saldo)
+
+	titular2 := "Luciene"
+	numAgencia2 := 555
+	numConta2 := 1113333
+	var saldo2 float64 = 200
+
+	fmt.Println(titular2, numAgencia2, numConta2, saldo2)
+}
